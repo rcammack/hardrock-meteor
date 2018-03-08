6da581b29b43ada5f25d8ef176752dce338eedc2
rcammack <rcammack@hawaii.edu>
2018-03-08 10:59:01 -1000

    Completed Hard Rock Meteor Mockup

--- app/imports/ui/pages/Hardrock.jsx
+++ app/imports/ui/pages/Hardrock.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import Middle from '../components/Middle';
-import Bottom from '../components/Bottom';
 
 export default class Hardrock extends React.Component {
   render() {
